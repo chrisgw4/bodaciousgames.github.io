4770b8e119fca1a19d56cce90f614d99d0474187
Christian <chrisgw4@gmail.com>
2025-01-03 20:10:10 -0500

    vanilla test

---
+++ assets/chunks/chunk-B3fOemxT.js
@@ -0,0 +1,1 @@
+import{r as a,j as o}from"./chunk-DkCFziec.js";const i="div",x=a.forwardRef(({tag:r=i,children:e,...s},t)=>o.jsx(r,{...s,ref:t,children:e}));x.displayName="Text";export{x as s};
--- assets/chunks/chunk-DTmgi2v7.js
+++
@@ -1,1 +0,0 @@
-import{r as a,j as o}from"./chunk-dkObpX_l.js";const i="div",x=a.forwardRef(({tag:r=i,children:e,...s},t)=>o.jsx(r,{...s,ref:t,children:e}));x.displayName="Text";export{x as s};
